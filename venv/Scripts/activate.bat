acace660c595f73fd539c912a20351a55070619c
Thibault <laudethibault83@gmail.com>
2022-07-21 10:12:52 +0200

    Ajout du gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\manon\OneDrive\Documents\GitHub\scrum\venv
+set VIRTUAL_ENV=C:\Users\ipi-pcpo-006\Documents\GitHub\scrum\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
