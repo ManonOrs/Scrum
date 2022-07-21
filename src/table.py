33f059e6d2216fed5065068ba72079c0b5f99006
ManonOrs <manon.orsini5@gmail.com>
2022-07-21 10:03:54 +0200

    création de la classe Table

---
+++ src/table.py
@@ -0,0 +1,28 @@
+class Table:
+    table_number = 0
+    seats = 0
+    status = 'libre'
+    clients = 0
+    orders = []
+
+    def __init__(self, table_number, seats, status, clients, orders):
+        self.table_number = table_number
+        self.seats = seats
+        self.status = status
+        self.clients = clients
+        self.orders = orders
+
+    def set_status(self, status):
+        self.status = status
+
+    def add_clients(self, clients_number):
+        self.clients = self.clients + clients_number
+
+    def remove_client(self, clients_number):
+        self.clients = self.clients - clients_number
+
+    def add_order(self, order):
+        self.orders.append(order)
+
+    def remove_order(self, order):
+        self.orders.remove(order)
